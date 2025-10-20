fd772f5d9fee3b487bad30d1655df7a38f0144d7
Elijah Melton <elimelt@uw.edu>
2025-10-19 18:21:25 -0700

    qwer

--- grader/grade.sh
+++ grader/grade.sh
@@ -6,15 +6,15 @@ DATA=$1
 OUT=${2:-output}
 mkdir -p $OUT
 
-docker build -t cse447-proj/demo -f Dockerfile .
+docker build -t proj/demo -f Dockerfile .
 
 function run() {
-  docker run --rm \
+    docker run --rm \
     -v $PWD/src:/job/src \
     -v $PWD/work:/job/work \
     -v $DATA:/job/data \
     -v $PWD/$OUT:/job/output \
-    cse447-proj/demo \
+    proj/demo \
     bash /job/src/predict.sh /job/data/input.txt /job/output/pred.txt
 }
 
